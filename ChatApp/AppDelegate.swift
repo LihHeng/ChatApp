5c2be63b5c2530cdc833984ea4b54a14f33180ec
Ying Mei Lum <ymeiosis@gmail.com>
2018-02-12 17:57:16 +0800

    google code two-thirds done. check commented out part in login controller

--- ChatApp/AppDelegate.swift
+++ ChatApp/AppDelegate.swift
@@ -8,9 +8,27 @@
 
 import UIKit
 import Firebase
+import GoogleSignIn //1a. import
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
+    
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
+        //3
+        if let error = error {
+        return
+    }
+        guard let authentication = user.authentication else { return }
+        let credential = GoogleAuthProvider.credential(withIDToken: authentication.idToken, accessToken: authentication.accessToken)
+        // ...
+    }
+    
+    func sign(_ signIn: GIDSignIn!, didDisconnectWith user: GIDGoogleUser!, withError error: Error!) {
+        // Perform any operations when the user disconnects from app here.
+        // ...
+    }
+    
+    //1b. add delegate
 
     var window: UIWindow?
 
@@ -18,6 +36,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
+        GIDSignIn.sharedInstance().clientID = FirebaseApp.app()?.options.clientID //2.
+        GIDSignIn.sharedInstance().delegate = self //2.
         return true
     }
 
@@ -42,6 +62,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
+    
+    func application(_ application: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any])
+        -> Bool {return GIDSignIn.sharedInstance().handle(url, sourceApplication:options[UIApplicationOpenURLOptionsKey.sourceApplication] as? String, annotation: [:])
+    }
+    
 
 
 }
--- ChatApp/LoginViewController.swift
+++ ChatApp/LoginViewController.swift
@@ -8,53 +8,76 @@
 
 import UIKit
 import FirebaseAuth
+import GoogleSignIn
 
 class LoginViewController: UIViewController {
-    @IBOutlet weak var googleImageView: UIImageView!
     
-    @IBOutlet weak var facebookImageView: UIImageView!
-    
-    @IBOutlet weak var emailTextField: UITextField!
-    
-    @IBOutlet weak var passwordTextField: UITextField!
-    
-    @IBAction func nextButton(_ sender: Any) {
-        signInTapped()
+    @IBOutlet weak var googleImageView: GIDSignInButton! {
+        didSet {
+//            Auth.auth().signIn(with: credential) { (user, error) in
+//                if let error = error {
+//                    // ...
+//                    return
+//                }
+//            }
+//            
+//            
+//            Auth.auth().signIn(with: <#T##AuthCredential#>, completion: <#T##AuthResultCallback?##AuthResultCallback?##(User?, Error?) -> Void#>)
+        }
     }
-    
-    override func viewDidLoad() {
+        
+        
+        
+        @IBOutlet weak var emailTextField: UITextField!
+        
+        @IBOutlet weak var passwordTextField: UITextField!
+        
+        @IBAction func nextButton(_ sender: Any) {
+        signInTapped()
+        }
+        
+        override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         if Auth.auth().currentUser != nil {
-            guard let vc = storyboard?.instantiateViewController(withIdentifier: "navigationController") as? UINavigationController else {return}
-            
-            present(vc, animated: true, completion: nil)
+        guard let vc = storyboard?.instantiateViewController(withIdentifier: "navigationController") as? UINavigationController else {return}
+        
+        present(vc, animated: true, completion: nil)
         }
-    }
-    
-    func signInTapped() {
+//            GIDSignIn.sharedInstance().delegate = self as! GIDSignInDelegate
+//        GIDSignIn.sharedInstance().signIn()
+        }
+        
+        func signInTapped() {
         guard let email = emailTextField.text,
-            let password = passwordTextField.text else {return}
+        let password = passwordTextField.text else {return}
         
         Auth.auth().signIn(withEmail: email, password: password) { (user, error) in
-            if let validError = error {
-                //show Alert
-                self.showAlert(withTitle: "Error", message: validError.localizedDescription)
-            }
-            if let validUser = user {
-                guard let navVC = self.storyboard?.instantiateViewController(withIdentifier: "navigationController") as? UINavigationController else {return}
-                
-                self.present(navVC, animated: true, completion: nil)
-            }
+        if let validError = error {
+        //show Alert
+        self.showAlert(withTitle: "Error", message: validError.localizedDescription)
         }
-    }
-    
-    
-    override func didReceiveMemoryWarning() {
+        if let validUser = user {
+        guard let navVC = self.storyboard?.instantiateViewController(withIdentifier: "navigationController") as? UINavigationController else {return}
+        
+        self.present(navVC, animated: true, completion: nil)
+        }
+        }
+        }
+        
+        
+        override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        }
+        
+        
     }
 
-
+    
+extension LoginViewController: GIDSignInUIDelegate {
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
+        //do something
+        
+    }
 }
-
